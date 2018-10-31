8b8d08776119441c8b35637a52b229e87191d9af
Nazar Bodnar <nazar.bodnar@perfectial.com>
2018-10-31 19:53:11 +0200

    add life page

---
+++ tmp-life-settlements.php
@@ -0,0 +1,97 @@
+<?php
+/*
+Template Name: Life Settlements
+*/
+get_header(); ?>
+<main>
+	<div class="inner-top-baner bg-img">
+		<img src="<?php echo get_template_directory_uri(); ?>/images/life-bg.png" alt="">
+		<div class="container">
+			<div class="top-baner-box">
+				<div class="info white-bg-padding">
+					<p>Life Settlements</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="life-settlements">
+		<div class="container">
+			<div class="box border">
+				<div class="row">
+					<div class="col-md-4">
+						<h2>What is a life settlement?</h2>
+					</div>
+					<div class="col-md-8">
+						<p>A life settlement is the transfer of ownership and beneficiary rights of an unwanted or unneeded life insurance policy in exchange for a cash settlement. Life Settlements are based on the premise that many individuals no longer need or can afford to keep paying premiums on their insurance policies. The seller no longer has the responsibility of paying future premiums. In exchange, investors profit based on the difference of the face value of the policy and acquisition and maintenance costs. </p>
+						<div class="img">
+							<img src="<?php echo get_template_directory_uri(); ?>/images/pay.png" alt="">
+						</div>
+						<p>The secondary market landscape is dominated by insureds typically aged 65 and older, some with medical impairments resulting in shortened life expectancies. </p>
+						<p>Life Settlements became more sophisticated as actuarial techniques and advanced financial analysis were implemented during the underwriting process. With the senior population adhering itself to traditional underwriting guidelines, the Life Settlement industry could then leverage of the existing foundation of the life insurance industry and with a supply of life insurance policies that was exponentially larger has grown into a multi-billion dollar market. The industry today is almost entirely comprised of a few mainstream capital sources that have revolutionized the speed and scale of the secondary insurance market. The industry has also become more specialized with seniors working with financial advisors, brokers and a number of ancillary and service providers.</p>
+					</div>
+				</div>
+			</div>
+			<div class="box border">
+				<div class="row">
+					<div class="col-md-4">
+						<h2>Life Settlement Market</h2>
+					</div>
+					<div class="col-md-8">
+						<p>Nobody knows the Life Settlement market like our managers. As our funds continue to expand, we are creating even more efficient strategies to bring Life Settlements to market. These strategies include supporting legislative and regulatory proposals favorable to our expansion while strengthening the rights of owners and buyers. As the largest aging population in United States history is poised for exponential growth in the next decade, consumers realize the mutually beneficial opportunity for decades to come.</p>
+					</div>
+				</div>
+			</div>
+			<div class="box border">
+				<div class="row">
+					<div class="col-md-4">
+						<h2>Life Settlement Regulation</h2>
+					</div>
+					<div class="col-md-8">
+						<p>The Life Settlement industry is highly regulated at the state level. There are some common features of United States insurance law which create a sound foundation for secondary life insurance market growth in the United States. These common features explain why the origination platform for life settlements is located primarily in the United States.</p>
+						<p>Many states in the US have introduced regulations leading investors to view life settlements with greater confidence.  This regulation is aimed at creating more efficiency in the marketplace and providing both buyers and sellers with a highly structured transaction </p>
+					</div>
+				</div>
+			</div>
+			<div class="box border">
+				<div class="row">
+					<div class="col-md-4">
+						<h2>Tertiary Market of Life Insurance</h2>
+					</div>
+					<div class="col-md-8">
+						<p>Since 2009, the industry has developed a tertiary market for life insurance (secondary market for life settlements) which involves a number of investors, trading blocks of life settlements amongst each other. This tertiary market continues to grow to record transaction volumes and provides additional liquidity to the life settlement asset class. As volume increases in tertiary transactions, so does the liquidity profile of the asset class and the attractiveness of the opportunity to large, sophisticated institutional investors.</p>
+					</div>
+				</div>
+			</div>
+			<div class="box border">
+				<div class="row">
+					<div class="col-md-4">
+						<h2>Investment Process</h2>
+					</div>
+					<div class="col-md-8">
+						<p>In the Luxembourg Life Funds, our managers have not only pioneered various aspects of the Life Settlement market, but have invested significant time, money and resources in creating direct origination relationships. Our managers utilize long-standing partnerships and marketing facilities to provide investors with the most efficient and profitable policy acquisition strategies. As a result, our Funds underwrite only the highest investment grade assets while realizing higher margins on transactions.</p>
+						<p>Carlisle Management Company’s success in marketing comes from years of experience, strategy testing, monitoring and refining our approach to the life settlement industry. Our market intelligence as well as top-quality team allows us to anticipate changes, identify new sources of policies and participate in the market as a leader. Our team relies heavily on effective communication strategies to reduce new customer acquisition expenses, maintain transparency and boost return on capital invested.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="contact-us">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="contact-text">
+							<h2>Contact Us</h2>
+							<p>We offer unique options to a diverse range of investors.</p>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="contact-btn">
+							<a class="default-btn" href="">contact us</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+</main>
+
+<?php get_footer(); ?>
